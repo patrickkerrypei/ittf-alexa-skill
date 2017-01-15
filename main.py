3692c275e130668c63901461908bd47916506e9c
Patrick <patrickkerry.pei@gmail.com>
2017-01-15 01:32:29 -0600

    Added phrase detection

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-# from venv/lib/flask import Flask
-# from flask-ask import Ask, statement
-
 from flask import Flask
-from flask_ask import Ask, statement
+from flask_ask import Ask, statement, question, convert_errors
 
 app = Flask(__name__)
-ask = Ask(app, "/")
+ask = Ask(app, '/')
 
-@ask.intent("HelloIntent")
+
+@ask.launch
+def launched():
+    introduction = 'Hello, you can ask me about Men\'s and Women\'s table tennis world rankings. ' \
+                   'In the future, I will support more complex queries.'
+    return statement(introduction)
+
+
+@ask.intent('HelloIntent')
 def hello():
-    return statement("Success")
+    introduction = 'Hello, you can ask me about Men\'s and Women\'s table tennis world rankings. ' \
+                   'In the future, I will support more complex queries.'
+    return statement(introduction)
+
+
+@ask.intent('PlayerRankingIntent',
+            mapping={'rank': 'Rank',
+                     'gender': 'Gender'},
+            convert={'rank': int})
+def player_ranking(rank, gender):
+    return statement('Received rank number {} and gender {}'.format(rank, gender))
+
+
+def get_single_player_ranking(rank, gender):
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
